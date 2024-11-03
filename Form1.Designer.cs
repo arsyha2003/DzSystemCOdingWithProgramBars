2392dc884262df6d749ff3675c548cea54ea5299
arsyha2003 <arsenyghoul@gmail.com>
2024-11-03 13:17:36 +0700

    сделано

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(244, 523);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 29);
+            button1.TabIndex = 6;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += StartProgressBarButton;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 525);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(191, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 491);
+            label1.Name = "label1";
+            label1.Size = new Size(205, 20);
+            label1.TabIndex = 8;
+            label1.Text = "Количество прогресс баров";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(944, 563);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,51 @@
+using System;
+
 namespace DzSystemCodingPBars
 {
     public partial class Form1 : Form
     {
+        List<ProgressBar> progressBarList;
         public Form1()
         {
             InitializeComponent();
+            progressBarList = new List<ProgressBar>();
+
+        }
+        private void StartProgressBarButton(object sender, EventArgs e)
+        {
+            try
+            {
+                int count = int.Parse(textBox1.Text);
+                for (int i = 0; i < count; i++)
+                {
+                    ProgressBar progressBar = new ProgressBar
+                    {
+                        Location = new Point(10, 50 + i * 30),
+                        Size = new Size(350, 20)
+                    };
+                    this.Controls.Add(progressBar);
+                    progressBarList.Add(progressBar);
+                    Task.Run(() => StarProgressBars(progressBar));
+                }
+
+            }
+            catch { }
+        
+        }
+        private void Form1_Load(object sender, EventArgs e){ }
+        private async Task StarProgressBars(ProgressBar progressBar)
+        {
+            await Task.Run(() =>
+            {
+                int speed = new Random().Next(10, 100);
+                Color randomColor = Color.FromArgb(new Random().Next(256), new Random().Next(256), new Random().Next(256));
+                while (progressBar.Value < 100)
+                {
+                    progressBar.Value = Math.Min(progressBar.Value + new Random().Next(1, 10),100);
+                    progressBar.ForeColor = randomColor;
+                    Thread.Sleep(speed);
+                }
+            });
         }
     }
 }
