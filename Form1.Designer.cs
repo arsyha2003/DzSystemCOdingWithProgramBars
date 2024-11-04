c07b43133ef405b3f8cd06c825dc53da01abea54
arsyha2003 <arsenyghoul@gmail.com>
2024-11-04 18:58:24 +0700

    сделано

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 34);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(287, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(197, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Введите путь к директории";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(358, 72);
+            button1.Name = "button1";
+            button1.Size = new Size(428, 103);
+            button1.TabIndex = 2;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += StartButton;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 110);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(287, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(174, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Введите искомое слово";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(358, 39);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(428, 27);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(358, 16);
+            label3.Name = "label3";
+            label3.Size = new Size(182, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Где было найдено слово";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(944, 563);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,52 @@ namespace DzSystemCodingPBars
 {
     public partial class Form1 : Form
     {
+        public string dirPath = string.Empty;
+        public string searchWord = string.Empty;
         public Form1()
         {
             InitializeComponent();
+        }
+        public async void StartButton(object sender, EventArgs e)
+        {
+            try
+            {
+                dirPath = textBox1.Text;
+                searchWord = textBox2.Text;
+            }
+            catch { MessageBox.Show("все плёхо"); }
+            Task t1 = Task.Run(() => SearchWordInFiles());
+        }
+        public async Task SearchWordInFiles()
+        {
+            await Task.Run(() =>
+            {
+                var files = Directory.EnumerateFiles(dirPath, "*.*", SearchOption.AllDirectories);
+                var tasks = new List<Task>();
+                foreach (var file in files)
+                {
+                    tasks.Add(ProcessFile(file));
+                }
+                Task.WhenAll(tasks);
+            });
+        }
+        public async Task ProcessFile(string filePath)
+        {
+            try
+            {
+                using (var reader = new StreamReader(filePath))
+                {
+                    string content = await reader.ReadToEndAsync();
+                    if (content.Contains(searchWord, StringComparison.OrdinalIgnoreCase))
+                    {
+                        textBox3.Text = $"Слово {searchWord} найдено в файле: {filePath}";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при обработке файла {filePath}: {ex.Message}");
+            }
         }
     }
 }
