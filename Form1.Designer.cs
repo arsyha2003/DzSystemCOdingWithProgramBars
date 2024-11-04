5853cb599244cc90b0752ed5222b0b83d58bffb1
arsyha2003 <arsenyghoul@gmail.com>
2024-11-04 18:29:43 +0700

    сделано

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(105, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(105, 64);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Range1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Range2";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(692, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(220, 544);
+            listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 112);
+            button1.Name = "button1";
+            button1.Size = new Size(181, 29);
+            button1.TabIndex = 5;
+            button1.Text = "StartFibonacci";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += StartFibonacciButton;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(944, 563);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private ListBox listBox1;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,36 @@ namespace DzSystemCodingPBars
         {
             InitializeComponent();
         }
+        public async void StartFibonacciButton(object sender, EventArgs e)
+        {
+            Task t1 = Task.Run(() => StartFibonacci());
+        }
+        public async Task StartFibonacci()
+        {
+            await Task.Run(() =>
+            {
+                listBox1.Items.Clear();
+                ulong range1 = 0;
+                ulong range2 = 0;
+                try
+                {
+                    range1 = ulong.Parse(textBox1.Text);
+                    range2 = ulong.Parse(textBox2.Text);
+                }
+                catch { MessageBox.Show("Данные введены некорректно!");return; }
+                ulong num1 = range1;
+                ulong num2 = range1+1;
+                ulong num3 =0;
+                for (int i = (int)range1; i < (int)range2; i++)
+                {
+                    num3 = num1 + num2;
+                    num1 = num2;
+                    listBox1.Items.Add(num3);
+                    num2 = num3;
+                } 
+                    
+
+            });
+        }
     }
 }
