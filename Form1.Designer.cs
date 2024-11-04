0b6fc632a82b495f4c87994f79ab7e97c12ac8bc
arsyha2003 <arsenyghoul@gmail.com>
2024-11-04 18:45:52 +0700

    сделано

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(30, 50);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(174, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(174, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Введите искомое слово";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(245, 49);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(173, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(245, 82);
+            button1.Name = "button1";
+            button1.Size = new Size(173, 79);
+            button1.TabIndex = 3;
+            button1.Text = "Расчитать количество вхождений слова";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += CounterOfLettersButton;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(944, 563);
+            ClientSize = new Size(474, 252);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,42 @@ namespace DzSystemCodingPBars
         {
             InitializeComponent();
         }
+        public async void CounterOfLettersButton(object sender,EventArgs e)
+        {
+            Task t1 = Task.Run(() => CounterOfLetters());
+        }
+        public async Task CounterOfLetters()
+        {
+            await Task.Run(() =>
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                int count = 0;
+                string filePath = fd.FileName;
+                string lines;
+                string userLetter = textBox1.Text;
+                if (!filePath.ToLower().Contains(".txt")) { MessageBox.Show("Вы выбрали не текстовый файл!!");return; }
+                using(FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    using(StreamReader sr = new StreamReader(fs))
+                    {
+                        lines = sr.ReadToEnd();
+                    }
+                }
+                string[] letters = lines.Split(new char[] { ' ', '.', ',', '!', '?' });//можно еще по другим знакам сплитить, я основные выписал
+                foreach(string letter in letters)
+                {
+                    if(letter.Length>userLetter.Length)
+                    {
+                        if (letter.ToLower().Contains(userLetter.ToLower())) count++;
+                    }
+                    else
+                    {
+                        if (userLetter.ToLower().Contains(letter.ToLower())) count++;
+                    }
+                }
+                textBox2.Text = $"Количество вхождений: {count}";
+            });
+        }
     }
 }
